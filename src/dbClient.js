5b31ad016f9cdf5b3a793f595481a90ab4748ff3
Vyborov Sergey <Sergey.Vyborov@bercut.com>
2020-05-03 22:46:28 +0300

    Add db methods to work with stats

--- src/dbClient.js
+++ src/dbClient.js
@@ -46,25 +46,7 @@ module.exports = class DBClient {
       .toArray();
   }
 
-  async saveAchieve(achieve) {
-    const mongo = this._mongo();
-    const client = await mongo.connect();
-    await client
-      .db(this.dbName)
-      .collection('achieves')
-      .updateOne(
-        { "_id": achieve._id },
-        { $set: achieve },
-        { upsert :true }
-        );
-    await client.close();
-  }
-
-  async getAllTodayChats() {
-    const now = new Date();
-    const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-    const timestamp = startOfDay / 1000;
-
+  async getMessagesStatByDate(timestamp) {
     const mongo = this._mongo();
     const client = await mongo.connect();
     return client
@@ -75,13 +57,41 @@ module.exports = class DBClient {
           {$match: {"date": {$gt: timestamp}}},
           {
             $group: {
-              _id: "$chat.id",
-              title: {"$first": "$chat.title"},
-              type: {"$first": "$chat.type"},
+              _id: {
+                "chat_id": "$chat.id",
+                "user_id": "$from.id",
+              },
+              count: {"$sum": 1},
+              first_name: {"$first": "$from.first_name"},
+              last_name: {"$first": "$from.last_name"},
+            }
+          },
+          {
+            $group: {
+              _id: "$_id.chat_id",
+              stats: {
+                $push: {
+                  id: "$_id.user_id",
+                  count: "$count",
+                  first_name: "$first_name",
+                  last_name: "$last_name",
+                }
+              }
             }
           }
         ]
       )
+      .toArray();
+
+  }
+
+  async getAllChats() {
+    const mongo = this._mongo();
+    const client = await mongo.connect();
+    return client
+      .db(this.dbName)
+      .collection('chats')
+      .find()
       .toArray();
   }
 
@@ -94,18 +104,7 @@ module.exports = class DBClient {
       .findOne({"chatId": chatId, "achieveId": achieveId});
   }
 
-  async getUnusedMessages() {
-    const mongo = this._mongo();
-    const client = await mongo.connect();
-    return client
-      .db(this.dbName)
-      .collection('messages')
-      .find({'used': {$ne: true}})
-      .toArray();
-  }
-
   async getChat(chatId) {
-    console.log('getChat:: ', chatId);
     const mongo = this._mongo();
     const client = await mongo.connect();
     return client
@@ -115,29 +114,27 @@ module.exports = class DBClient {
   }
 
   async saveChat(chat) {
-    console.log('saveChat', chat)
     const mongo = this._mongo();
     const client = await mongo.connect();
     return client
       .db(this.dbName)
       .collection('chats')
       .updateOne(
-        { "_id": chat._id },
-        { $set: chat },
-        { upsert :true }
+        {"_id": chat._id},
+        {$set: chat},
+        {upsert: true}
       );
   }
 
-  async setMessageUsed(messageId) {
-    console.log(messageId);
+  async resetChatStat(chatId, statType) {
     const mongo = this._mongo();
     const client = await mongo.connect();
     return client
       .db(this.dbName)
-      .collection('messages')
+      .collection('chats')
       .updateOne(
-        { "_id": messageId },
-        { $set: { "used": true } }
+        {"_id": chatId},
+        {$set: {[statType]: []}},
       );
   }
 };
