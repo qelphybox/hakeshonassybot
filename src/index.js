4723d37322644c0eb1e4d8b8f08ba02514896a8d
Vyborov Sergey <Sergey.Vyborov@bercut.com>
2020-05-03 22:47:57 +0300

    Rewrite /stats message

--- src/index.js
+++ src/index.js
@@ -14,17 +14,19 @@ const client = new DBClient(
   process.env['MONGO_DB_NAME']
 );
 
+const getUserStatString = (row) => `${row.first_name} ${row.last_name} (${row.count})`;
+
 // Register listeners
 slimbot.on('message', async message => {
   if (message.text.startsWith('/stats')) {
     const chatId = message.chat.id;
     const chat = await client.getChat(chatId);
     // todo: в общей статистике есть только id пользователя, за всей статистикой надо все равно лезть в юзеров
-    const text = `Сообщений за ${moment().format('DD.MM.YYYY')} : ${
-      chat.messages_count.map(row => {
-        const user = chat.users.find((user) => user.id === row.user_id);
-        return `${user.first_name} ${user.last_name} (${row.count})`
-      }).join("\n")
+    const text = `Сообщений за ${moment().format('DD.MM.YYYY')}: ${
+      chat.today_message_count.map(getUserStatString).join("\n")
+    }
+Сообщений за последний час: ${
+      chat.hour_message_count.map(getUserStatString).join("\n")
     }`;
     slimbot.sendMessage(chatId, text)
   } else if (message.text.startsWith('/mupd')) {
@@ -35,7 +37,6 @@ slimbot.on('message', async message => {
     const text = `${achieves[0].achieveName}: ${achieves[0].first_name} ${achieves[0].last_name}`
     slimbot.sendMessage(chatId, text);
   } else {
-    message.used = false;
     client.saveMessage(message);
   }
 });
