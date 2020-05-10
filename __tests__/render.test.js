b1f7965b73d3a4de7af9e7443a0239ca35f03fa8
Vyborov Sergey <Sergey.Vyborov@bercut.com>
2020-05-10 20:53:44 +0300

    add tests for render functions

---
+++ __tests__/render.test.js
@@ -0,0 +1,23 @@
+const {stats} =  require('../src/stats');
+
+const { getUserStatString, renderMessage } = require('../src/render');
+
+describe('getUserStatString', () => {
+  test('username: test, count: 2', () => {
+    expect(getUserStatString({ username: 'test', count: 2})).toBe('@test (2)');
+  });
+});
+
+
+describe('renderMessage', () => {
+  test('TODAY_MESSAGE_COUNT', () => {
+    const statsArray = [{ name: stats.TODAY_MESSAGE_COUNT, data: [{username: 'test', count: 1}, {username: 'test2', count: 2}] }]
+    expect(renderMessage(statsArray)).toBe(`Сообщений за последние 24 часа: @test (1), @test2 (2)`);
+  });
+
+  test('HOUR_MESSAGE_COUNT', () => {
+    const statsArray = [{ name: stats.HOUR_MESSAGE_COUNT, data: [{username: 'test', count: 1}, {username: 'test2', count: 2}] }]
+    expect(renderMessage(statsArray)).toBe('Сообщений за последний час: @test (1), @test2 (2)');
+  });
+});
+
