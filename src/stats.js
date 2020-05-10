f76837a273e89b922263b221393e7d7de64e1016
Vyborov Sergey <Sergey.Vyborov@bercut.com>
2020-05-10 20:50:28 +0300

    add message timestamp to stats functions

--- src/stats.js
+++ src/stats.js
@@ -13,19 +13,23 @@ const client = new DBClient(
 );
 
 
-const statByDay = async (chatId) => {
-  const now = new Date();
-  const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const dayTimestamp = startOfDay / 1000;
-  const data = (await client.getChatMessagesStatByDate(chatId, dayTimestamp));
+const statByDay = async (statsRequestObj) => {
+  const { chatId, messageTimestamp } = statsRequestObj;
+  const messageDate = new Date(messageTimestamp);
+  messageDate.setDate(messageDate.getDate() - 1);
+  const dayTimestamp = messageDate / 1000;
+  const data = await client.getChatMessagesStatByDate(chatId, dayTimestamp);
+  console.log(data)
   return { name: stats.TODAY_MESSAGE_COUNT, data };
 };
 
-const statByHour = async (chatId) => {
-  const now = new Date();
-  now.setHours(now.getHours() - 1);
-  const hourTimestamp = Math.floor(now / 1000);
-  const data = (await client.getChatMessagesStatByDate(chatId, hourTimestamp));
+const statByHour = async (statsRequestObj) => {
+  const { chatId, messageTimestamp } = statsRequestObj;
+  const messageDate = new Date(messageTimestamp);
+  messageDate.setHours(messageDate.getHours() - 1);
+  const hourTimestamp = Math.floor(messageDate / 1000);
+  const data = await client.getChatMessagesStatByDate(chatId, hourTimestamp);
+  console.log(data)
   return  { name: stats.HOUR_MESSAGE_COUNT, data };
 };
 
