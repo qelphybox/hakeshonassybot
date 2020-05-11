bd0b59aea159d73a73067547df3a9157bed4ade9
Vyborov Sergey <Sergey.Vyborov@bercut.com>
2020-05-12 01:00:18 +0300

    add render template to worklessUser

--- src/render.js
+++ src/render.js
@@ -5,6 +5,13 @@ const getUserStatString = (row) => `@${row.username} (${row.count})`;
 const templates = {
   [stats.TODAY_MESSAGE_COUNT]: (stat) => `Сообщений за последние 24 часа: ${stat.map(getUserStatString).join(", ")}`,
   [stats.HOUR_MESSAGE_COUNT]: (stat) => `Сообщений за последний час: ${stat.map(getUserStatString).join(", ")}`,
+  [stats.WORKLESS_USER]: (stat) => {
+    if (stat.length > 0)  {
+      return `${stat[0].username} - безработный`
+    } else {
+      return ''
+    }
+  },
 };
 
 const renderMessage = (statsArray) => {
