8a12a1f054087968227826e4dfd94b45eac971f0
Vyborov Sergey <Sergey.Vyborov@bercut.com>
2020-05-12 00:58:34 +0300

    add dbclient function getWorklessUser

--- src/dbClient.js
+++ src/dbClient.js
@@ -41,4 +41,39 @@ module.exports = class DBClient {
       )
       .toArray();
   }
+
+  async getWorklessUser(chatId, timestamp) {
+    const mongo = this._mongo();
+    const client = await mongo.connect();
+    return client
+      .db(this.dbName)
+      .collection('messages')
+      .aggregate(
+        [
+          {$match: {"chat.id": chatId, "date": {$gt: timestamp}}},
+          {
+            $match: {
+              $expr: {
+                $in: [{
+                  $dayOfWeek: {
+                    $toDate: {$multiply: ["$date", 1000]}
+                  }
+                }, [2, 3, 4, 5, 6]]
+              }
+            }
+          },
+          {$match: {$expr: {$and: [{$gt: [{$hour: {$toDate: {$multiply: ["$date", 1000]}}}, 10]}, {$lt: [{$hour: {$toDate: {$multiply: ["$date", 1000]}}}, 18]}]}}},
+          {
+            $group: {
+              _id: "$from.id",
+              count: {"$sum": 1},
+              username: {"$first": "$from.username"},
+            }
+          },
+          {$sort: {count: -1}},
+          {$limit: 1}
+        ]
+      )
+      .toArray();
+  }
 };
