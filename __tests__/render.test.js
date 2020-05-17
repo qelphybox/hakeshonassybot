6f05de8c8b0d500e43a3f2d761835d0148fd991a
Vyborov Sergey <Sergey.Vyborov@bercut.com>
2020-05-17 17:16:22 +0300

    add tests for empty data, refactoring tests

--- __tests__/render.test.js
+++ __tests__/render.test.js
@@ -1,10 +1,10 @@
-const {stats} = require('../src/stats');
+const { stats } = require('../src/stats');
 
-const {getUserStatString, renderMessage} = require('../src/render');
+const { getUserStatString, renderMessage } = require('../src/render');
 
 describe('getUserStatString', () => {
   test('username: test, count: 2', () => {
-    expect(getUserStatString({username: 'test', count: 2})).toBe('@test (2)');
+    expect(getUserStatString({ username: 'test', count: 2 })).toBe('@test (2)');
   });
 });
 
@@ -13,39 +13,38 @@ describe('renderMessage', () => {
   test('TODAY_MESSAGE_COUNT', () => {
     const statsArray = [{
       name: stats.TODAY_MESSAGE_COUNT,
-      data: [{username: 'test', count: 1}, {username: 'test2', count: 2}]
-    }]
-    expect(renderMessage(statsArray)).toBe(`Сообщений за последние 24 часа: @test (1), @test2 (2)`);
+      data: [{ username: 'test', count: 1 }, { username: 'test2', count: 2 }],
+    }];
+    expect(renderMessage(statsArray)).toBe('Сообщений за последние 24 часа: @test (1), @test2 (2)');
   });
 
   test('HOUR_MESSAGE_COUNT', () => {
     const statsArray = [
       {
         name: stats.TODAY_MESSAGE_COUNT,
-        data: [{username: 'test', count: 1}, {username: 'test2', count: 2}]
+        data: [{ username: 'test', count: 1 }, { username: 'test2', count: 2 }],
       },
       {
         name: stats.HOUR_MESSAGE_COUNT,
-        data: [{username: 'test', count: 1}, {username: 'test2', count: 2}]
-      }
-    ]
+        data: [{ username: 'test', count: 1 }, { username: 'test2', count: 2 }],
+      },
+    ];
     expect(renderMessage(statsArray)).toBe(
-      'Сообщений за последние 24 часа: @test (1), @test2 (2)\n' +
-      'Сообщений за последний час: @test (1), @test2 (2)'
+      'Сообщений за последние 24 часа: @test (1), @test2 (2)\n'
+      + 'Сообщений за последний час: @test (1), @test2 (2)',
     );
   });
 
   test('HOUR_MESSAGE_COUNT and TODAY_MESSAGE_COUNT', () => {
     const statsArray = [{
       name: stats.HOUR_MESSAGE_COUNT,
-      data: [{username: 'test', count: 1}, {username: 'test2', count: 2}]
-    }]
+      data: [{ username: 'test', count: 1 }, { username: 'test2', count: 2 }],
+    }];
     expect(renderMessage(statsArray)).toBe('Сообщений за последний час: @test (1), @test2 (2)');
   });
 
   test('empty array', () => {
-    const statsArray = []
+    const statsArray = [];
     expect(renderMessage(statsArray)).toBe('');
   });
 });
-
