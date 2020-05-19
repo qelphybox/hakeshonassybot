729db8d0fbf536009fd3a77bbca9ffc044719da9
Kirill Bobykin <qelphybox@gmail.com>
2020-05-19 19:06:03 +0300

    make dbClient singleton

--- src/index.js
+++ src/index.js
@@ -1,51 +1,50 @@
-const Slimbot = require('slimbot');
 const { renderMessage } = require('./render');
 const { statFunctions } = require('./stats');
 
-const DBClient = require('./dbClient');
-
+const Slimbot = require('slimbot');
+const { dbClient } = require('./dbClient');
 
 const buildProxySettings = () => {
-  let socks5;
-  if (process.env.SOCKS5_HOST || process.env.SOCKS5_PORT) {
-    socks5 = {
-      socksHost: process.env.SOCKS5_HOST,
-      socksPort: process.env.SOCKS5_PORT,
-    };
+  if (process.env['SOCKS5_HOST'] || process.env['SOCKS5_PORT']) {
+    return {
+      socksHost: process.env['SOCKS5_HOST'],
+      socksPort: process.env['SOCKS5_PORT'],
+    }
   }
-  return socks5;
-};
-const slimbot = new Slimbot(process.env.TELEGRAM_BOT_TOKEN, buildProxySettings());
+}
 
-const client = new DBClient(
-  process.env.MONGO_URL,
-  process.env.MONGO_DB_NAME,
-);
-
+const slimbot = new Slimbot(process.env['TELEGRAM_BOT_TOKEN'], buildProxySettings());
 
 const stats = async (statsRequestObj) => {
   const sendStats = await Promise.all(
     Object.keys(statFunctions)
-      .map((statFunctionName) => statFunctions[statFunctionName](statsRequestObj)),
+      .map((statFunctionName) => statFunctions[statFunctionName](statsRequestObj))
   );
 
   const text = renderMessage(sendStats);
-  slimbot.sendMessage(
-    statsRequestObj.chatId,
-    text,
-    { disable_web_page_preview: true, disable_notification: true, parse_mode: 'Markdown' },
-  );
+  slimbot.sendMessage(statsRequestObj.chatId, text, { disable_web_page_preview: true, disable_notification: true });
 };
 
 // Register listeners
-slimbot.on('message', async (message) => {
+slimbot.on('message', async message => {
   if (message.text.startsWith('/stats')) {
     const chatId = message.chat.id;
     const messageTimestamp = message.date;
-    stats({ chatId, messageTimestamp });
+    stats({ chatId, messageTimestamp, dbClient });
   } else {
-    client.saveMessage(message);
+    dbClient.queryMessages((messages) => {
+      messages.insertOne(message)
+    }) ;
   }
 });
 
-slimbot.startPolling();
+dbClient.connect().then(() => {
+  slimbot.startPolling();
+})
+
+process.on('exit', async (code) => {
+  console.log(`Exit with code ${code}, stopping...`);
+  await dbClient.disconnect();
+  slimbot.stopPolling();
+  console.log('Bye!');
+})
--- src/stats.js
+++ src/stats.js
@@ -1,5 +1,4 @@
-const DBClient = require('./dbClient');
-
+const { dbClient } = require('./dbClient');
 
 const stats = {
   TODAY_MESSAGE_COUNT: 'today_message_count',
@@ -7,44 +6,102 @@ const stats = {
   WORKLESS_USER: 'workless_user',
 };
 
-
-const client = new DBClient(
-  process.env.MONGO_URL,
-  process.env.MONGO_DB_NAME,
-);
-
-
-const statByDay = async ({ chatId, messageTimestamp }) => {
+const statByDay = async ({chatId, messageTimestamp}) => {
   const messageDate = new Date(messageTimestamp * 1000);
   messageDate.setDate(messageDate.getDate() - 1);
   const dayTimestamp = messageDate / 1000;
-  const data = await client.getChatMessagesStatByDate(chatId, dayTimestamp);
-  return { name: stats.TODAY_MESSAGE_COUNT, data };
+  const data = await dbClient.queryMessages(messages => {
+    return messages.aggregate(
+      [
+        {$match: {"chat.id": chatId, "date": {$gt: dayTimestamp}}},
+        {
+          $group: {
+            _id: "$from.id",
+            count: {"$sum": 1},
+            username: {"$first": "$from.username"},
+          }
+        },
+        {$sort: {count: -1}}
+      ]
+    ).toArray();
+  });
+  return {name: stats.TODAY_MESSAGE_COUNT, data};
 };
 
-const statByHour = async ({ chatId, messageTimestamp }) => {
+const statByHour = async ({chatId, messageTimestamp}) => {
   const messageDate = new Date(messageTimestamp * 1000);
   messageDate.setHours(messageDate.getHours() - 1);
   const hourTimestamp = Math.floor(messageDate / 1000);
-  const data = await client.getChatMessagesStatByDate(chatId, hourTimestamp);
-  return { name: stats.HOUR_MESSAGE_COUNT, data };
+  const data = await dbClient.queryMessages(messages => {
+    return messages.aggregate(
+      [
+        {$match: {"chat.id": chatId, "date": {$gt: hourTimestamp}}},
+        {
+          $group: {
+            _id: "$from.id",
+            count: {"$sum": 1},
+            username: {"$first": "$from.username"},
+          }
+        },
+        {$sort: {count: -1}}
+      ]
+    ).toArray();
+  });
+  return {name: stats.HOUR_MESSAGE_COUNT, data};
 };
 
-const worklessUser = async ({ chatId, messageTimestamp }) => {
+const worklessUser = async ({chatId, messageTimestamp}) => {
   const messageDate = new Date(messageTimestamp * 1000);
   const mondayNumber = 1;
   const currentDay = messageDate.getDay();
   messageDate.setDate(messageDate.getDate() - (currentDay - mondayNumber));
   const dayTimestamp = messageDate / 1000;
-  const data = await client.getWorklessUser(chatId, dayTimestamp);
-  return { name: stats.WORKLESS_USER, data };
-};
+  const data = await dbClient.queryMessages(messages => {
+    return messages.aggregate(
+      [
+        {$match: {"chat.id": chatId, "date": {$gt: dayTimestamp}}},
+        {
+          $project: {
+            _id: 1,
+            "from.id": 1,
+            "from.username": 1,
+            dayOfWeek: {
+              $dayOfWeek: {
+                $toDate: {$multiply: ["$date", 1000]}
+              }
+            },
+            hour: {$hour: {$toDate: {$multiply: ["$date", 1000]}}}
+          }
+        },
+        {
+          $match: {
+            $expr: {
+              $in: ["$dayOfWeek", [2, 3, 4, 5, 6]]
+            }
+          }
+        },
+        {$match: {$expr: {$and: [{$gte: ["$hour", 10]}, {$lt: ["$hour", 18]}]}}},
+        {
+          $group: {
+            _id: "$from.id",
+            count: {"$sum": 1},
+            username: {"$first": "$from.username"},
+          }
+        },
+        {$sort: {count: -1}},
+        {$limit: 1}
+      ]
+    )
+      .toArray();
+  })
+  return {name: stats.WORKLESS_USER, data};
+}
 
 const statFunctions = {
   statByDay,
   statByHour,
   worklessUser,
-};
+}
 
 module.exports = {
   stats,
