17afe7907959c52deb21661e0968b246dd351641
svyborov <sergey-vyborov@yandex.ru>
2020-05-24 22:00:15 +0300

    remove user link from stat message

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,8 +1,9 @@
 /* eslint-disable camelcase */
 const getUserLink = ({ first_name, last_name, _id }) => `[${first_name}${last_name ? ` ${last_name}` : ''}](tg://user?id=${_id})`;
+const getFullUserName = ({ first_name, last_name }) => `${first_name}${last_name ? ` ${last_name}` : ''}`;
 const getUserStatString = ({
-  first_name, last_name, count, _id,
-}) => `${getUserLink({ first_name, last_name, _id })} (${count})`;
+  first_name, last_name, count,
+}) => `${getFullUserName({ first_name, last_name })} (${count})`;
 /* eslint-enable camelcase */
 
 const renderMessage = (statsStringsArray) => statsStringsArray
@@ -10,4 +11,6 @@ const renderMessage = (statsStringsArray) => statsStringsArray
   .join('\n');
 
 
-module.exports = { getUserLink, getUserStatString, renderMessage };
+module.exports = {
+  getUserLink, getUserStatString, renderMessage, getFullUserName,
+};
