cf82b13fcbeb9caf896ebf3398b74ac58bd6b4fc
svyborov <sergey-vyborov@yandex.ru>
2020-05-24 16:42:37 +0300

    put stat tests on different files

---
+++ __tests__/hourMessageCountStat.test.js
@@ -0,0 +1,52 @@
+// hours timestamp search = 1589133600
+
+const hourMessageCountStat = require('../src/statistics/hourMessageCountStat');
+
+const { describeDBSetupTeardown } = require('./lib/dbHelper');
+const { dbClient } = require('../src/dbClient');
+const messagesByHour = require('./__fixtures__/messagesByHourFixtures/correctData.json');
+
+describeDBSetupTeardown();
+
+const addMessages = async (messages) => {
+  await dbClient.queryMessages((col) => col.insertMany(messages));
+};
+
+describe('statByHour', () => {
+  test('statByHour', async () => {
+    await addMessages(messagesByHour);
+    const collection = await hourMessageCountStat
+      .collect({ chat: { id: 1 }, date: 1589133600 });
+    expect(collection).toEqual(
+      [
+        {
+          _id: 2,
+          count: 3,
+          first_name: 'test2',
+          last_name: 'test2',
+          username: 'test2',
+        },
+        {
+          _id: 1,
+          count: 2,
+          first_name: 'test1',
+          last_name: 'test1',
+          username: 'test1',
+        },
+      ],
+    );
+
+    const statString = hourMessageCountStat.render(collection);
+    expect(statString).toBe('Сообщений за последний час: [test2 test2](tg://user?id=2) (3), [test1 test1](tg://user?id=1) (2)');
+  });
+
+  test('empty data', async () => {
+    const collection = await hourMessageCountStat
+      .collect({ chat: { id: 1 }, date: 1589133600 });
+    expect(collection).toEqual([]);
+
+    const statString = hourMessageCountStat.render(collection);
+
+    expect(statString).toBe('Сообщений за последний час: ');
+  });
+});
---
+++ __tests__/worklessUserStat.test.js
@@ -0,0 +1,40 @@
+const worklessUserStat = require('../src/statistics/worklessUserStat');
+
+const { describeDBSetupTeardown } = require('./lib/dbHelper');
+const { dbClient } = require('../src/dbClient');
+const messagesWorklessUser = require('./__fixtures__/messagesWorklessUser/correctData.json');
+
+describeDBSetupTeardown();
+
+const addMessages = async (messages) => {
+  await dbClient.queryMessages((col) => col.insertMany(messages));
+};
+describe('worklessUser', () => {
+  test('worklessUser', async () => {
+    await addMessages(messagesWorklessUser);
+    const collection = await worklessUserStat.collect({ chat: { id: 1 }, date: 1588982400 });
+    expect(collection).toEqual(
+      [{
+        _id: 2,
+        count: 3,
+        first_name: 'test2',
+        last_name: 'test2',
+        username: 'test2',
+      }],
+    );
+
+    const statString = worklessUserStat.render(collection);
+
+    expect(statString).toBe('[test2 test2](tg://user?id=2) - безработный');
+  });
+
+  test('empty data', async () => {
+    const collection = await worklessUserStat.collect({ chat: { id: 1 }, date: 1588982400 });
+    expect(collection).toEqual([]);
+
+
+    const statString = worklessUserStat.render(collection);
+
+    expect(statString).toBe('');
+  });
+});
