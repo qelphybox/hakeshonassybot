da6e4130938254763fa7815d33e20597c45040c3
svyborov <sergey-vyborov@yandex.ru>
2020-06-11 20:36:15 +0300

    refactor slimbot mock

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -1,13 +1,8 @@
 jest.mock('./__mocks__/slimbot');
-const MockSlimbot = require('slimbot');
 const { onMessage } = require('../src/actions');
 const { dbClient } = require('../src/dbClient');
 
 const { describeDBSetupTeardown } = require('./lib/dbHelper');
-
-
-describeDBSetupTeardown();
-
 const messagesWorklessUser = require('./__fixtures__/messagesWorklessUser/correctData.json');
 const messagesContentSupplier = require('./__fixtures__/messagesContentSupplier/correctData.json');
 
@@ -17,14 +12,17 @@ const addMessages = async (messages) => {
   await dbClient.queryMessages((col) => col.insertMany(messages));
 };
 
-describe('onMessage', () => {
-  test('empty data', () => new Promise((done) => {
-    const testCallback = (text) => {
-      expect(text).toBe('Сообщений за последние 24 часа: \n'
-        + 'Сообщений за последний час: ');
-      done();
-    };
-    const slimbot = new MockSlimbot(testCallback);
+const createMockedSlimbot = (expectedText) => ({
+  sendMessage: jest.fn((chatId, text) => {
+    expect(text).toMatch(expectedText);
+  }),
+});
+
+describe('auto create messages', () => {
+  test('empty data', async () => {
+    const slimbot = createMockedSlimbot('Сообщений за последние 24 часа: \n'
+      + 'Сообщений за последний час: ');
+
     const message = {
       chat: {
         id: 0,
@@ -33,51 +31,45 @@ describe('onMessage', () => {
       entities: [{ type: 'bot_command' }],
     };
 
-    onMessage(slimbot, message);
-  }));
+    await onMessage(slimbot, message);
+  });
 
-  test('messagesContentSupplier', () => new Promise((done) => {
-    addMessages(messagesContentSupplier).then(() => {
-      const testCallback = (text) => {
-        expect(text).toBe('Сообщений за последние 24 часа: test1 test1 (2)\n'
-          + 'Сообщений за последний час: test1 test1 (2)\n'
-          + 'test1 test1 - безработный\n'
-          + 'test1 test1 - поставщик контента');
-        done();
-      };
-      const slimbot = new MockSlimbot(testCallback);
-      const message = {
-        chat: {
-          id: 1,
-        },
-        date: 1588982400,
-        text: '/stats',
-        entities: [{ type: 'bot_command' }],
-      };
+  test('messagesContentSupplier', async () => {
+    await addMessages(messagesContentSupplier);
 
-      onMessage(slimbot, message);
-    });
-  }));
+    const slimbot = createMockedSlimbot('Сообщений за последние 24 часа: test1 test1 (2)\n'
+      + 'Сообщений за последний час: test1 test1 (2)\n'
+      + 'test1 test1 - безработный\n'
+      + 'test1 test1 - поставщик контента');
 
-  test('messagesWorklessUser', () => new Promise((done) => {
-    addMessages(messagesWorklessUser).then(() => {
-      const testCallback = (text) => {
-        expect(text).toBe('Сообщений за последние 24 часа: test1 test1 (2)\n'
-          + 'Сообщений за последний час: test1 test1 (2)\n'
-          + 'test2 test2 - безработный');
-        done();
-      };
-      const slimbot = new MockSlimbot(testCallback);
-      const message = {
-        chat: {
-          id: 1,
-        },
-        date: 1588982400,
-        text: '/stats',
-        entities: [{ type: 'bot_command' }],
-      };
+    const message = {
+      chat: {
+        id: 1,
+      },
+      date: 1588982400,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
 
-      onMessage(slimbot, message);
-    });
-  }));
+    await onMessage(slimbot, message);
+  });
+
+  test('messagesWorklessUser', async () => {
+    await addMessages(messagesWorklessUser);
+
+    const slimbot = createMockedSlimbot('Сообщений за последние 24 часа: test1 test1 (2)\n'
+      + 'Сообщений за последний час: test1 test1 (2)\n'
+      + 'test2 test2 - безработный');
+
+    const message = {
+      chat: {
+        id: 1,
+      },
+      date: 1588982400,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
+
+    await onMessage(slimbot, message);
+  });
 });
