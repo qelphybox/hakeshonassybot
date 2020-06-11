a7e679ba3f6f108ee7206e9f5648d16fd921e084
svyborov <sergey-vyborov@yandex.ru>
2020-06-11 20:36:45 +0300

    add manual create messages tests

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -1,6 +1,7 @@
 jest.mock('./__mocks__/slimbot');
 const { onMessage } = require('../src/actions');
 const { dbClient } = require('../src/dbClient');
+const { sendTestMessage } = require('./lib/sendMessageHelper');
 
 const { describeDBSetupTeardown } = require('./lib/dbHelper');
 const messagesWorklessUser = require('./__fixtures__/messagesWorklessUser/correctData.json');
@@ -71,5 +72,168 @@ describe('auto create messages', () => {
     };
 
     await onMessage(slimbot, message);
+  });
+});
+
+describe('manual create messages', () => {
+  test('workless user', async () => {
+    const slimbot = createMockedSlimbot('user1 - безработный');
+
+    await sendTestMessage({
+      userId: 1, firstName: 'user1', date: 1591786800, type: 'text',
+    }, slimbot);
+    await sendTestMessage({
+      userId: 1, firstName: 'user1', date: 1591786800, type: 'text',
+    }, slimbot);
+
+    await sendTestMessage({
+      userId: 2, firstName: 'user2', date: 1591790400, type: 'text',
+    }, slimbot);
+
+    await sendTestMessage({
+      userId: 3, firstName: 'user3', date: 1591815600, type: 'text',
+    }, slimbot);
+    await sendTestMessage({
+      userId: 3, firstName: 'user3', date: 1591815600, type: 'text',
+    }, slimbot);
+    await sendTestMessage({
+      userId: 3, firstName: 'user3', date: 1591815600, type: 'text',
+    }, slimbot);
+
+    const statMessage = {
+      chat: {
+        id: 1,
+      },
+      date: 1591887600,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
+
+    await onMessage(slimbot, statMessage);
+  });
+
+
+  test('worst user', async () => {
+    const slimbot = createMockedSlimbot('user2 - худший юзер чата');
+
+    await sendTestMessage({
+      userId: 1, firstName: 'user1', date: 1591786800, type: 'voice',
+    }, slimbot);
+
+    await sendTestMessage({
+      userId: 2, firstName: 'user2', date: 1591790400, type: 'voice',
+    }, slimbot);
+    await sendTestMessage({
+      userId: 2, firstName: 'user2', date: 1591790400, type: 'voice',
+    }, slimbot);
+
+    const statMessage = {
+      chat: {
+        id: 1,
+      },
+      date: 1591887600,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
+
+    await onMessage(slimbot, statMessage);
+  });
+
+  test('content supplier', async () => {
+    const slimbot = createMockedSlimbot('user2 - поставщик контента');
+
+    await sendTestMessage({
+      userId: 1, firstName: 'user1', date: 1591786800, type: 'photo',
+    }, slimbot);
+
+    await sendTestMessage({
+      userId: 2, firstName: 'user2', date: 1591790400, type: 'photo',
+    }, slimbot);
+    await sendTestMessage({
+      userId: 2, firstName: 'user2', date: 1591790400, type: 'video',
+    }, slimbot);
+
+    const statMessage = {
+      chat: {
+        id: 1,
+      },
+      date: 1591887600,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
+
+    await onMessage(slimbot, statMessage);
+  });
+
+  test('24 hours', async () => {
+    const slimbot = createMockedSlimbot('Сообщений за последние 24 часа: user1 (23), user2 (11), user3 (8)');
+
+    const sendMessagesArray = [];
+    for (let i = 1591833600; i < 1591920000; i += 3600) {
+      sendMessagesArray.push(sendTestMessage({
+        userId: 1, firstName: 'user1', date: i, type: 'text',
+      }, slimbot));
+    }
+
+    for (let i = 1591833600; i < 1591920000; i += 7200) {
+      sendMessagesArray.push(sendTestMessage({
+        userId: 2, firstName: 'user2', date: i, type: 'text',
+      }, slimbot));
+    }
+
+    for (let i = 1591833600; i < 1591920000; i += 10600) {
+      sendMessagesArray.push(sendTestMessage({
+        userId: 3, firstName: 'user3', date: i, type: 'text',
+      }, slimbot));
+    }
+
+    await Promise.all(sendMessagesArray);
+
+    const statMessage = {
+      chat: {
+        id: 1,
+      },
+      date: 1591920000,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
+
+    await onMessage(slimbot, statMessage);
+  });
+
+  test('one hour', async () => {
+    const slimbot = createMockedSlimbot('Сообщений за последний час: user1 (59), user2 (5), user3 (2)');
+
+    const sendMessagesArray = [];
+    for (let i = 1591833600; i < 1591837200; i += 60) {
+      sendMessagesArray.push(sendTestMessage({
+        userId: 1, firstName: 'user1', date: i, type: 'text',
+      }, slimbot));
+    }
+
+    for (let i = 1591833600; i < 1591837200; i += 600) {
+      sendMessagesArray.push(sendTestMessage({
+        userId: 2, firstName: 'user2', date: i, type: 'text',
+      }, slimbot));
+    }
+
+    for (let i = 1591833600; i < 1591837200; i += 1200) {
+      sendMessagesArray.push(sendTestMessage({
+        userId: 3, firstName: 'user3', date: i, type: 'text',
+      }, slimbot));
+    }
+
+    await Promise.all(sendMessagesArray);
+
+    const statMessage = {
+      chat: {
+        id: 1,
+      },
+      date: 1591837200,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
+
+    await onMessage(slimbot, statMessage);
   });
 });
