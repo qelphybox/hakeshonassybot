871c5b77eec0e8db539b8c40d5201fc81abf6f88
svyborov <sergey-vyborov@yandex.ru>
2020-07-20 23:49:43 +0300

    add new tests for stickerPackerStat

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -517,4 +517,40 @@ describe('manual create messages', () => {
 
     await onMessage(slimbot, statMessage);
   });
+
+  test('sticker packer', async () => {
+    const slimbot = createMockedSlimbot((chatId, text) => {
+      expect(text).toMatch('*user1* - стикерпакер');
+    });
+
+    await sendTestMessage({
+      userId: 1, firstName: 'user1', date: 1591866060, type: 'sticker',
+    }, onMessage, slimbot, { setName: 'Pojelaniepchelki' });
+    await sendTestMessage({
+      userId: 1, firstName: 'user1', date: 1591866060, type: 'sticker',
+    }, onMessage, slimbot, { setName: 'ultrarjombav2' });
+
+    await sendTestMessage({
+      userId: 2, firstName: 'user2', date: 1591866060, type: 'sticker',
+    }, onMessage, slimbot, { setName: 'ultrarjombav2' });
+
+    await sendTestMessage({
+      userId: 2, firstName: 'user2', date: 1591866060, type: 'sticker',
+    }, onMessage, slimbot, { setName: 'ultrarjombav2' });
+
+    await sendTestMessage({
+      userId: 2, firstName: 'user2', date: 1591866060, type: 'sticker',
+    }, onMessage, slimbot, { setName: 'ultrarjombav2' });
+
+    const statMessage = {
+      chat: {
+        id: 1,
+      },
+      date: 1592222400,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
+
+    await onMessage(slimbot, statMessage);
+  });
 });
