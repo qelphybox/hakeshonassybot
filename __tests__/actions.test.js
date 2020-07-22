ccd82a7e767ad1fea9814cd359c120153a673a5a
svyborov <sergey-vyborov@yandex.ru>
2020-07-23 00:25:46 +0300

    add new test for maybeDiedStat to check days

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+const MockDate = require('mockdate');
 const { onMessage } = require('../src/actions');
 const { dbClient } = require('../src/dbClient');
 const { sendTestMessage } = require('./lib/sendMessageHelper');
@@ -554,8 +556,10 @@ describe('manual create messages', () => {
   });
 
   test('maybe died', async () => {
+    MockDate.set(moment('06/23/2020').toDate());
+
     const slimbot = createMockedSlimbot((chatId, text) => {
-      expect(text).toMatch('*user1* - наверное помер');
+      expect(text).toMatch('*user1* - наверное помер (писал 12 дня назад)');
     });
 
     await sendTestMessage({
@@ -577,6 +581,29 @@ describe('manual create messages', () => {
     }, onMessage, slimbot);
     await sendTestMessage({
       userId: 3, firstName: 'user3', date: 1591787000, type: 'text',
+    }, onMessage, slimbot);
+
+    const statMessage = {
+      chat: {
+        id: 1,
+      },
+      date: 1591887600,
+      text: '/stats',
+      entities: [{ type: 'bot_command' }],
+    };
+
+    await onMessage(slimbot, statMessage);
+  });
+
+  test('maybe died (less than one day)', async () => {
+    MockDate.set(moment('06/10/2020').toDate());
+
+    const slimbot = createMockedSlimbot((chatId, text) => {
+      expect(text).toMatch('*user1* - наверное помер (писал 1 дня назад)');
+    });
+
+    await sendTestMessage({
+      userId: 1, firstName: 'user1', date: 1591786800, type: 'text',
     }, onMessage, slimbot);
 
     const statMessage = {
