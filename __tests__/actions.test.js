1b43d7bb2706b0419babe323fcc0dde8f85d3457
svyborov <sergey-vyborov@yandex.ru>
2020-10-19 00:47:30 +0300

    add tests for onMessageEdit action

--- __tests__/actions.test.js
+++ __tests__/actions.test.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 const MockDate = require('mockdate');
-const { onMessage } = require('../src/actions');
+const { onMessage, onMessageEdit } = require('../src/actions');
 const { dbClient } = require('../src/dbClient');
 const { sendTestMessage } = require('./lib/sendMessageHelper');
 
@@ -792,5 +792,58 @@ describe('manual create messages', () => {
     };
 
     await onMessage(slimbot, rickMessage);
+  });
+});
+
+describe('on message edit action', () => {
+  test('workless user', async () => {
+    const chat = {
+      id: -379023065,
+      title: 'hakeshonassybot_develop_bot',
+      type: 'group',
+      all_members_are_administrators: true,
+    };
+
+    const firstMessage = {
+      message_id: 503,
+      from: {
+        id: 309091867,
+        is_bot: false,
+        first_name: 'Sergey',
+        last_name: 'Vyborov',
+        username: 'svyborov',
+        language_code: 'en',
+      },
+      chat,
+      date: 1603056894,
+      text: 'test1',
+    };
+
+    await onMessage(null, firstMessage);
+    await dbClient.queryMessages(async (col) => {
+      const message = await col.findOne({ message_id: firstMessage.message_id });
+      expect(message).toEqual(firstMessage);
+    });
+    const editedMessage = {
+      message_id: 503,
+      from: {
+        id: 309091867,
+        is_bot: false,
+        first_name: 'Sergey1',
+        last_name: 'Vyborov1',
+        username: 'svyborov',
+        language_code: 'en',
+      },
+      chat,
+      date: 1603056895,
+      edit_date: 1603056911,
+      text: 'test2',
+    };
+    await onMessageEdit(null, editedMessage);
+
+    await dbClient.queryMessages(async (col) => {
+      const message = await col.findOne({ message_id: firstMessage.message_id });
+      expect(message).toMatchObject(editedMessage);
+    });
   });
 });
