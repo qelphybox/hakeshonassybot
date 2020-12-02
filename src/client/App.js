2676a14f42fdf64ec966888a8f22b2d89e27c8ed
Kirill Bobykin <qelphybox@gmail.com>
2020-12-02 12:07:11 +0300

    fixed welcome markup

--- src/client/App.js
+++ src/client/App.js
@@ -5,6 +5,7 @@ import Container from 'react-bulma-components/lib/components/container';
 import Content from 'react-bulma-components/lib/components/content';
 import Hero from 'react-bulma-components/lib/components/hero';
 import Heading from 'react-bulma-components/src/components/heading';
+import Columns from 'react-bulma-components/src/components/columns';
 import WelcomeImage from './welcome.svg';
 
 function App() {
@@ -13,14 +14,18 @@ function App() {
       <Hero size="fullheight">
         <Hero.Head renderAs="header" />
         <Hero.Body>
-          <Container style={ { display: 'flex' } }>
-            <div style={ {
-              display: 'flex', alignContent: 'center', justifyContent: 'center', flexDirection: 'column',
-            } }>
-              <Heading size={1} renderAs="p">HakeShonassyBot</Heading>
-              <Heading subtitle renderAs="p">Телеграм бот раздающий глупые ачивки участникам чата</Heading>
-            </div>
-            <img src={WelcomeImage} alt='welcome image'/>
+          <Container>
+            <Columns>
+              <Columns.Column style={ { display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <div>
+                  <Heading size={1} renderAs="p" className="has-text-centered-mobile is-size-2-mobile">HakeShonassyBot</Heading>
+                  <Heading subtitle renderAs="p" className="has-text-centered-mobile is-size-5-mobile">Телеграм бот раздающий глупые ачивки участникам чата</Heading>
+                </div>
+              </Columns.Column>
+              <Columns.Column>
+                <img src={WelcomeImage} alt='welcome image'/>
+              </Columns.Column>
+            </Columns>
           </Container>
         </Hero.Body>
         <Hero.Footer>
