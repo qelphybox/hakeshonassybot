58a40a3bd7196c93bdd019309feb66c889ea7585
svyborov <sergey-vyborov@yandex.ru>
2021-03-23 23:29:42 +0300

    fix

--- src/bot/index.js
+++ src/bot/index.js
@@ -7,20 +7,19 @@ const { dbClient } = require('../dbClient');
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 
 // Register listeners
-bot.on("message", onMessage.bind(null, bot));
+bot.on('message', onMessage.bind(null, bot));
 
 bot.on('edited_message', onMessageEdit.bind(null, bot));
 
 dbClient.connect()
   .then(() => {
     bot.startPolling();
-
   })
   .catch((e) => {
     console.error('Database connection error', e);
   });
 
-process.on('exit', async (code) => {
+process.on("exit", async (code) => {
   console.log(`Exit with code ${code}, stopping...`);
   bot.stopPolling();
   await dbClient.disconnect();
