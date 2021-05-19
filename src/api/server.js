8013a9f892f14d308dfff92736ce2c82ceb1305d
smthingwicked <spartak.71.1994@rambler.ru>
2021-05-20 00:13:57 +0300

    add get('/api/stupid_achievments')

--- src/api/server.js
+++ src/api/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const path = require('path');
+const AuthService = require('./services/auth.service');
 
 const app = express();
 const authRouter = require('./routes/auth.router');
@@ -28,6 +29,30 @@ app.get('/test-auth', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../public/index.html'));
   res.status(200);
+});
+
+app.get('/api/stupid_achievments', authRouter, (req, res) => {
+  const user = { ...req.query };
+  if (AuthService.validateTelegramAuth(user)) {
+    res.send({
+      status: 'ok',
+      stupid_achievments: [
+        { title: 'Количество сообщений', name: 'messages_count' },
+        { title: 'Безработный', name: 'workless_user' },
+        { title: 'Поставщик контента', name: 'content_supplier' },
+        { title: 'Худший юзер чата', name: 'worst_chat_user' },
+        { title: 'Стикерпакер', name: 'stickerpacker' },
+        { title: 'Пропавший без вести', name: 'maybe_died' },
+        { title: 'Дудь', name: 'dud' },
+        { title: 'Философ', name: 'philosopher' },
+        { title: 'Юморист', name: 'humorist' },
+      ],
+    });
+    res.status(200);
+  } else {
+    res.send({ status: 'forbidden' });
+    res.status(403);
+  }
 });
 
 // catch 404 and forward to error handler
