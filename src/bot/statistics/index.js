c5ffff815cf3345d4dbf6920e0e8b04abfc6606b
Sergey <39708082+svyborov@users.noreply.github.com>
2021-11-27 00:56:52 +0300

    add content supplier stat (#556)
    Co-authored-by: Kirill Bobykin <qelphybox@gmail.com>

--- src/bot/statistics/index.js
+++ src/bot/statistics/index.js
@@ -1,11 +1,13 @@
 const todayMessageCountStat = require('./todayMessageCountStat');
 const hourMessageCountStat = require('./hourMessageCountStat');
 const worklessUserStat = require('./worklessUserStat');
+const contentSupplierStat = require('./contentSupplierStat');
 
 const statsArray = [
   todayMessageCountStat,
   hourMessageCountStat,
   worklessUserStat,
+  contentSupplierStat,
 ];
 
 module.exports = { statsArray };
